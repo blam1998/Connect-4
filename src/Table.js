cd8efe0b12b3d935e4c8bd3e126f534a6e9f018b
blam1998 <57881360+blam1998@users.noreply.github.com>
2022-11-17 19:52:57 -0800

    Added the core functionality of the game.

--- src/Table.js
+++ src/Table.js
@@ -1,30 +1,119 @@
 import './Table.css';
+import {Component} from 'react';
 
 
-let TableDimensions = Array.from(Array(6), () => new Array(1,2,3,4,5,6,7));
 
-function GenerateTable(props){
+function Squares(props){
     return(
-        <table>
-            <tbody>
-            {props.TableDimensions.map((rElement, rIndex) => {
-                return(<tr className = {"row" + (rIndex + 1).toString()} key = {rIndex.toString()}>
-                    {rElement.map((cElement, cIndex) => {
-                        return (<td className = {"column" + (cIndex + 1).toString()} key = {cIndex.toString()}></td>)
-                    })}
-                </tr>)
-            })}
-            </tbody>
-        </table>
+        <div className = "square" onClick = {props.handleClick}>
+            {props.value}
+        </div>
     )
 }
 
-function Table(props){
-    return(
-    <div className = "Table-Div">
-        <GenerateTable TableDimensions = {TableDimensions} />
-    </div>
-    )
+class Table extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            table : Array.from(Array(6), () => new Array(7).fill(null)),
+            xIsNext : true,
+            hasWinner: false,
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.renderSquare = this.renderSquare.bind(this);
+    }
+
+    handleClick(row,column){
+        //If tile is already taken, ignore click.
+        if (this.state.hasWinner){
+            return;
+        }
+
+        if (this.state.table[row][column] != null){
+            return;
+        }
+        if (row + 1 < 6 && this.state.table[row + 1][column] === null){
+            return;
+        }
+
+        const tempTable = this.state.table.slice();
+        tempTable[row][column] = this.state.xIsNext? "X" : "O";
+        
+        if (calculateWinner(tempTable, row, column)) {
+            console.log(calculateWinner(tempTable, row, column) + " Wins");
+            this.setState({ hasWinner : !this.state.hasWinner});
+        }
+
+        this.setState({
+            table: tempTable,
+            xIsNext : !this.state.xIsNext
+        });
+
+    }
+
+    renderSquare(row, column){
+        return(
+            <Squares row = {row} column = {column} value = {this.state.table[row][column]} handleClick = {() => this.handleClick(row,column)} />
+        )
+    }
+
+    render(){
+        return(
+            <div className = "Game-Table">
+                {this.state.table.map((rElement, rIndex) => {
+                    return(
+                        <div className = "row">
+                            {rElement.map((cElement,cIndex) =>{
+                                return(this.renderSquare(rIndex, cIndex));
+                            })}
+                        </div>
+                    )
+                })}
+
+            </div>
+        )
+    }
+}
+
+
+function calculateWinner(table, row, column){
+    const rowLimit = 6;
+    const colLimit = 7;
+    const currChar = table[row][column];
+
+    const directions = 
+    [
+    [[1,0], [-1,0]], //Up and down
+    [[0,1], [0,-1]], //right and left
+    [[1,1], [-1,-1]], //left diagonal
+    [[-1,1], [1,-1]] //right diagonal
+    ]
+    
+
+    for (let i = 0; i < directions.length; i++){
+        for (let j = 0; j < directions[i].length; j++){
+            var currRow = directions[i][j][0];
+            var currCol = directions[i][j][1];
+            var tempRow = row + currRow;
+            var tempCol = column + currCol;
+            var count = 1;
+
+            while (tempRow < rowLimit && tempCol < colLimit && tempRow >= 0 && tempCol >= 0){
+                if (table[tempRow][tempCol] === currChar){
+                    count++
+                }
+                else{
+                    break;
+                }
+                if (count === 4){
+                    return currChar;
+                }
+                tempRow += currRow;
+                tempCol += currCol;
+            }
+        }
+    }
+    return null;
 }
 
 export {Table as Table};
